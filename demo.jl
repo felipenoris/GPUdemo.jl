b86fd7fe277e34cd3af4b6eb6de800fbe33c3b04
Felipe Noronha <felipenoris@gmail.com>
2019-05-07 14:53:40 -0300

    demo

---
+++ demo.jl
@@ -0,0 +1,36 @@
+
+using CuArrays, GPUArrays
+using BenchmarkTools
+
+function threadded_map!(f::Function, A::Array, B::Array)
+    Threads.@threads for i in 1:length(A)
+        A[i] = f(B[i])
+    end
+  A
+end
+x, y = rand(10^7), rand(10^7)
+kernel(y) = (y / 33f0) * (732.f0/y)
+# on the cpu without threads:
+single_t = @belapsed map!($kernel, $x, $y)
+
+# "on the CPU with 4 threads (2 real cores):
+thread_t = @belapsed threadded_map!($kernel, $x, $y)
+
+# on the GPU:
+xgpu, ygpu = cu(x), cu(y)
+gpu_t = @belapsed begin
+  map!($kernel, $xgpu, $ygpu)
+  GPUArrays.synchronize($xgpu)
+end
+times = [single_t, thread_t, gpu_t]
+speedup = maximum(times) ./ times
+println("speedup: $speedup")
+bar(, speedup, legend = false, fillcolor = :grey, ylabel = "speedup")
+
+using UnicodePlots
+
+plt = barplot( ["1 core", "2 cores", "gpu"],
+              speedup,
+        title="Benchmark (seconds)")
+
+show(plt)
